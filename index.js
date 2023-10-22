7e672d1a2e2e61eb0493cedf885f9ed7d863bdad
Keith Cullinan <kfcullinan@gmail.com>
2023-10-22 11:37:40 -0500

    added init funciton

--- index.js
+++ index.js
@@ -82,3 +82,16 @@ function flushOperation(intBuffer) {
                       break;
     }
   }
+
+  function rerender() {
+    screen.innerText = buffer;
+  }
+  function init() {
+    document
+        .querySelector(".calc-buttons")
+        .addEventListener("click", function (event) {
+            buttonClick(event.target.innerText)
+        });
+  }
+
+  init();
